ab043ba1a8f7b45dc731fb55dbae920eb3c9fdb9
Zhuldyz <zhulya711@gmail.com>
2023-12-06 17:59:00 +0300

    Программа удаляет 2-ю цифру из 3-знач.числа

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());   //256
+
+if(num>99 && num<1000)    //   && - и   || - или
+{
+int num1= num /100;   //  2
+int num3 = num % 10;   //  6
+int result = num1*10 + num3;
+Console.WriteLine(result);
+}
+else
+{
+Console.WriteLine("Число не подходит");
+}
